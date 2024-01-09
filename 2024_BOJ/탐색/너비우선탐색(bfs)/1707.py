adbeacf1dae7778e9c1901339957061ae4b3c0ef
celbeing <kimsd1983@naver.com>
2024-01-10 00:31:58 +0900

    24.01.05.

---
+++ 2024_BOJ/탐색/너비우선탐색(bfs)/1707.py
@@ -0,0 +1,41 @@
+#1707: 이분 그래프
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(graph, s):
+    dq = deque()
+    dq.append(s)
+    if check[s] == 0:
+        check[s] = 1
+
+    while dq:
+        v = dq.popleft()
+        for u in graph[v]:
+            if check[v] == check[u]:
+                return False
+            elif check[u] == 0:
+                dq.append(u)
+                check[u] = -check[v]
+    return True
+
+K = int(input())
+for k in range(K):
+    V,E = map(int,input().split())
+    graph = dict()
+    for v in range(1,V+1):
+        graph[v] = []
+    for e in range(E):
+        u,v = map(int,input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    check = [0 for v in range(V+1)]
+    biparite = True
+    for i in range(1,V+1):
+        if not(bfs(graph,i)):
+            biparite = False
+            break
+    if biparite:
+        print("YES")
+    else:
+        print("NO")
