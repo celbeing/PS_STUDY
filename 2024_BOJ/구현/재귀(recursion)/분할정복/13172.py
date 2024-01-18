94b5fb040418cc543f5ce3316d7468a8153ed09c
전라남도교육지원청 <kimsd1983@naver.com>
2024-01-18 21:27:09 +0900

    24.01.18.

--- 2024_BOJ/구현/재귀(recursion)/분할정복/13172.py
+++ 2024_BOJ/구현/재귀(recursion)/분할정복/13172.py
@@ -3,9 +3,27 @@ import sys
 from math import gcd
 input = sys.stdin.readline
 M = int(input())
+mod = 1000000007
+result = 0
+
+def comp(a,b,c):
+    res = 1
+    while b > 0:
+        if b % 2 == 1:
+            res *= a
+            res %= c
+            b -= 1
+        a *= a
+        a %= c
+        b //= 2
+    return res
+
 for _ in range(M):
     n,s = map(int,input().split())
     g = gcd(n,s)
     n //= g
     s //= g
-    print(n,s)
+    result += s*comp(n,mod-2,mod)%mod
+    result %= mod
+
+print(result)
