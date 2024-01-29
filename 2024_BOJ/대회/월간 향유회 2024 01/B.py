c3cc563659ec82d8291082a05d57a1f31ca58ad6
전라남도교육지원청 <kimsd1983@naver.com>
2024-01-29 10:28:31 +0900

    24.01.29.

--- 2024_BOJ/대회/월간 향유회 2024 01/B.py
+++ 2024_BOJ/대회/월간 향유회 2024 01/B.py
@@ -1,3 +1,22 @@
 #B번 - 캬루
 import sys
 input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    N, P = map(int,input().split())
+    dsum = 0
+    digit = [0]*N
+    t = 1
+    for i in range(N):
+        digit[i] = (P // t) % 10
+        dsum += digit[i]
+        t *= 10
+    lower = dsum % 3
+    upper = 3 - lower
+    t = 1
+    for i in range(N):
+        if digit[i] < upper:
+            print(*[P+t*upper, 3])
+        else:
+            print(*[P-t*lower, 3])
+        t *= 10
