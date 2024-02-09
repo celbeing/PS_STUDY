3a550ee284c750a2e2c961c6132c56b6049964c4
celbeing <kimsd1983@naver.com>
2024-02-09 12:02:22 +0900

    24.02.09.

---
+++ 2024_BOJ/풀어야 할 문제/1991.py
@@ -0,0 +1,11 @@
+#1991: 트리 순회
+import sys
+input = sys.stdin.readline
+N = int(input())
+tree = [[-1,-1,-1] for _ in range(26)]
+tree[0][0] = 0
+for _ in range(N):
+    node = list(input().split())
+    print(node)
+print(ord('.'))
+print(ord('A'))
