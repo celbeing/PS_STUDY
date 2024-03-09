6dfbdb095ac8f0b28fa90954ae4e02ec467f1d0f
전라남도교육지원청 <kimsd1983@naver.com>
2024-03-09 17:58:54 +0900

    24.03.09.

--- 2024_BOJ/동적계획법(dp)/누적합(prefix sum)/12911.py
+++ 2024_BOJ/동적계획법(dp)/누적합(prefix sum)/12911.py
@@ -1,11 +1,17 @@
 #12911 좋아하는 배열
 N,K = map(int,input().split())
-div = 1e9+7
+div = int(1e9+7)
 dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
 sum = [0 for _ in range(N+1)]
 for i in range(1,K+1): dp[1][i] = 1
 sum[1] = K
+
 for i in range(2, N+1):
     for j in range(1, K+1):
         dp[i][j] += sum[i-1]
-        
+        for k in range(j*2,K+1, j):
+            dp[i][j] -= dp[i-1][k]
+        sum[i] += dp[i][j]
+        sum[i] %= div
+
+print(sum[N]%div)
