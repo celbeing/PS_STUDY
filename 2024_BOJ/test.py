c4829b8f168270858fe3c5cb621a2feec5fd1519
celbeing <kimsd1983@naver.com>
2024-03-29 23:39:11 +0900

    24.03.29.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,29 +1,11 @@
-import time
-li = [0,1,1,1,0,0,1,1,0,0,1,1,1]
-res = []
-
-start = time.time()
-s,e = 0,0
-while s < len(li):
-    while li[s] == 0: s += 1
-    e = s
-    while e<len(li) and li[e] == 1: e += 1
-    res.append(e-s)
-    s = e
-print(res)
-print(f"실행시간 {time.time()-start:.10f}")
-
-start = time.time()
-li = [0,1,1,1,0,0,1,1,0,0,1,1,1]
-res = []
-cnt = 0
-for num in li:
-  if num:
-    cnt += 1
-  elif cnt:
-    res.append(cnt)
-    cnt = 0
-if cnt:
-  res.append(cnt)
-print(res)
-print(f"실행시간 {time.time()-start:.10f}")
+import sys
+input = sys.stdin.readline
+while True:
+    d = list(input().split())
+    if d[0] == '#': break
+    age = int(d[1])
+    weight = int(d[2])
+    if age > 17 or weight >= 80:
+        print(d[0],"Senior")
+    else:
+        print(d[0],"Junior")
