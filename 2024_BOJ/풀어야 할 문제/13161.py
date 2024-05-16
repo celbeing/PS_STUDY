702065c68f79bcf881420e6f914e50ec3d58c208
전라남도교육지원청 <kimsd1983@naver.com>
2024-05-16 21:23:29 +0900

    24.05.16.

--- 2024_BOJ/풀어야 할 문제/13161.py
+++ 2024_BOJ/풀어야 할 문제/13161.py
@@ -6,62 +6,58 @@ inf = int(1e9)
 
 N = int(input())
 camp = list(map(int,input().split()))
-capa = [[0]*(N+2)] + [[0]+list(map(int, input().split()))+[0] for _ in range(N)] + [[0]*(N+2)]
-flow = [[0]*(N+2) for _ in range(N+2)]
-
-for i in range(N):
-    if camp[i] == 1:
-        capa[0][i + 1] = inf
-        capa[i + 1][0] = inf
-    elif camp[i] == 2:
-        capa[N + 1][i + 1] = inf
-        capa[i + 1][N + 1] = inf
-
 graph = [[] for _ in range(N+2)]
-for i in range(N+2):
-    for j in range(N+2):
-        if capa[i][j] > 0: graph[i].append(j)
+for i in range(1,N+1):
+    if camp[i-1] == 1: graph[0].append((i,inf))
+    elif camp[i-1] == 2: graph[i].append((N+1,inf))
+    capa = list(map(int,input().split()))
+    for j in range(1,N+1):
+        if capa[j-1] > 0:
+            graph[i].append((j,capa[j-1]))
 
-# 유량 보내주기
-def make_flow(now,cut):
-    if now == N+1: return cut
-    for next in graph[now]:
-        residual = capa[now][next]-flow[now][next]
-        if level[now]+1 == level[next] and residual > 0:
-            if residual > cut: residual = cut
-            f = make_flow(next,residual)
-            if f > 0:
-                flow[now][next] += f
-                flow[next][now] -= f
-                return f
-    return 0
+flow = [[0]*(N+2) for _ in range(N+2)]
+work = [0]*(N+2)
+level = [-1]*(N+2)
 
 result = 0
 while True:
-    level = [-1] * (N + 2)
+    # 레벨 그래프 생성
+    for i in range(N+2): level[i] = -1
     level[0] = 0
     bfs = deque([0])
     while bfs:
         now = bfs.popleft()
-        for next in graph[now]:
-            if capa[now][next] - flow[now][next] > 0 and level[next] == -1:
+        for next,capa in graph[now]:
+            if level[next] == -1 and capa - flow[now][next] > 0:
                 level[next] = level[now] + 1
                 bfs.append(next)
     if level[-1] == -1: break
 
-    stack = [(0, inf)]
-    while stack:
-        now, cut = stack[-1]
-        for next in graph[now]:
-            residual = capa[now][next] - flow[now][next]
-            if level[now] + 1 == level[next] and residual > 0:
-        else:
-
-    # 최소 컷 탐색
+    # 유량 생성
+    for i in range(N+2): work[i] = 0
     while True:
-        f = make_flow(0,inf)
-        if f == 0: break
-        result += f
+        stack = [(0, inf)]
+        while stack:
+            now, cut = stack[-1]
+            if now == N + 1:
+                for i in range(len(stack) - 1):
+                    flow[stack[i][0]][stack[i + 1][0]] += cut
+                    flow[stack[i + 1][0]][stack[i][0]] -= cut
+                result += cut
+                break
+            for i in range(work[now], len(graph[now])):
+                work[now] += 1
+                next = graph[now][i][0]
+                capa = graph[now][i][1]
+                residual = capa - flow[now][next]
+                if residual > 0 and level[now] + 1 == level[next]:
+                    f = min(residual, cut)
+                    stack.append((next, f))
+                    break
+            else:
+                stack.pop()
+        else:
+            break
 
 print(result)
 
@@ -70,8 +66,8 @@ visited[0] = True
 bfs = deque([0])
 while bfs:
     now = bfs.popleft()
-    for next in graph[now]:
-        if not(visited[next]) and capa[now][next]-flow[now][next] > 0:
+    for next,capa in graph[now]:
+        if not(visited[next]) and capa-flow[now][next] > 0:
             visited[next] = True
             bfs.append(next)
 A = []
