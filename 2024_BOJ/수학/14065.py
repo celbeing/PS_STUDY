cda065f146ebc53aa4a96f23e115037b434c978c
celbeing <kimsd1983@naver.com>
2024-05-23 22:36:00 +0900

    24.05.23.

--- 2024_BOJ/수학/14065.py
+++
@@ -1,5 +0,0 @@
-#14065: Gorivo
-x = float(input())
-galon = 378.5411784
-mile = 1.609344
-print(galon/mile/x)
--- 2024_BOJ/수학/기하학/회전하는 캘리퍼스(rotating calipers)/10254.py
+++ 2024_BOJ/수학/기하학/회전하는 캘리퍼스(rotating calipers)/10254.py
@@ -11,12 +11,12 @@ def dist(a,b):
 def calipers(dots):
     left = []
     for p in dots:
-        while len(left) >= 2 and ccw(left[-2],left[-1],p) <= 0:
+        while len(left) >= 2 and ccw(left[-2],left[-1],p) < 0:
             left.pop()
         left.append(p)
     right = []
     for p in reversed(dots):
-        while len(right) >= 2 and ccw(right[-2],right[-1],p) <= 0:
+        while len(right) >= 2 and ccw(right[-2],right[-1],p) < 0:
             right.pop()
         right.append(p)
     convex = left[:-1] + right[:-1]
@@ -24,14 +24,15 @@ def calipers(dots):
     l,r = 0,0
     for i in range(n):
         if convex[i][1] < convex[l][1]: l = i
-        if convex[i][1] > convex[r][1]: r = i
+    for i in range(n-1,-1,-1):
+        if convex[i][1] >= convex[r][1]: r = i
     peek = dist(convex[l],convex[r])
-    result = []
+    result = convex[l]+convex[r]
     for i in range(n):
-        vec_l = [convex[(l+1)%n][0]-convex[l][0],convex[(l+1)%n][1]-convex[l][1]]
-        vec_r = [convex[r][0]-convex[(r+1)%n][0],convex[r][1]-convex[(r+1)%n][1]]
-        k = ccw([0,0],vec_l,vec_r)
-        if k > 0:
+        vec_l = (convex[(l+1)%n][0]-convex[l][0],convex[(l+1)%n][1]-convex[l][1])
+        vec_r = (convex[r][0]-convex[(r+1)%n][0],convex[r][1]-convex[(r+1)%n][1])
+        k = ccw((0,0),vec_l,vec_r)
+        if k >= 0:
             l = (l+1)%n
         else:
             r = (r+1)%n
@@ -44,7 +45,7 @@ def calipers(dots):
 T = int(input())
 for _ in range(T):
     n = int(input())
-    city = [list(map(int,input().split())) for _ in range(n)]
+    city = [tuple(map(int,input().split())) for _ in range(n)]
     city.sort()
     if n == 2:
         result = city[0]+city[1]
