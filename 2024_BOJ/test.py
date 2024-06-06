533f1e3d1735e7cebda9e2961e91d1e73e73dc9e
전라남도교육지원청 <kimsd1983@naver.com>
2024-06-06 17:01:42 +0900

    24.06.06.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,6 +1,93 @@
-people = list(map(int,input().split()))
-x,y,r = map(int,input().split())
-if x in people:
-    print(people.index(x)+1)
-else:
-    print(0)
+import sys
+from collections import deque, defaultdict
+
+input = sys.stdin.readline
+
+def connect(u, v, capacity=int(1e9)):
+    if v in graph[u]:
+        capa[u][v] += capacity
+    else:
+        graph[u].append(v)
+        graph[v].append(u)
+        capa[u][v] = capacity
+        capa[v][u] = 0
+        flow[u][v] = 0
+        flow[v][u] = 0
+
+def level_graph():
+    level = [-1] * (tt + 2)
+    level[0] = 0
+    queue = deque([0])
+    while queue:
+        current = queue.popleft()
+        for neighbor in graph[current]:
+            if level[neighbor] == -1 and capa[current][neighbor] - flow[current][neighbor] > 0:
+                queue.append(neighbor)
+                level[neighbor] = level[current] + 1
+    return level
+
+def dinic(now, flow_value):
+    if now == tt + 1:
+        return flow_value
+    while work[now] < len(graph[now]):
+        next_node = graph[now][work[now]]
+        residual_capacity = capa[now][next_node] - flow[now][next_node]
+        if level[now] + 1 == level[next_node] and residual_capacity > 0:
+            min_flow = dinic(next_node, min(flow_value, residual_capacity))
+            if min_flow > 0:
+                flow[now][next_node] += min_flow
+                flow[next_node][now] -= min_flow
+                return min_flow
+        work[now] += 1
+    return 0
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    tt = n * 100
+    start, goal, steps = map(int, input().split())
+    m = int(input())
+
+    graph = defaultdict(list)
+    capa = defaultdict(lambda: defaultdict(int))
+    flow = defaultdict(lambda: defaultdict(int))
+
+    for u in range(1, n + 1):
+        for v in range(-99, -99 + steps):
+            connect(u * 100 + v, u * 100 + v + 1)
+    connect(0, start * 100 - 99)
+
+    for _ in range(m):
+        hospital = int(input())
+        if hospital == start:
+            connect(0, tt + 1)
+        else:
+            for k in range(-99, -99 + steps):
+                connect(hospital * 100 + k, tt + 1)
+
+    for _ in range(int(input())):
+        a, b, p, t = map(int, input().split())
+        if a == start:
+            connect(0, b * 100 - 100 + t, p)
+        limit = min(-99 + steps, -t + 1)
+        for k in range(-99, limit):
+            connect(a * 100 + k, b * 100 + k + t, p)
+
+    total_flow = 0
+    while True:
+        level = level_graph()
+        if level[-1] == -1:
+            break
+
+        work = [0] * (tt + 2)
+        while True:
+            flow_value = dinic(0, int(1e9))
+            if flow_value == 0:
+                break
+            total_flow += flow_value
+            if total_flow >= goal:
+                break
+        if total_flow >= goal:
+            break
+
+    print(min(goal, total_flow))
---
+++ 2024_BOJ/풀어야 할 문제/10319.py
@@ -0,0 +1,88 @@
+# 10319: 좀비 아포칼립스
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def connect(u,v,k = int(1e9)):
+    if v in graph[u]:
+        capa[u][v] += k
+    else:
+        graph[u].append(v)
+        capa[u][v] = k
+        flow[u][v] = 0
+        graph[v].append(u)
+        capa[v][u] = 0
+        flow[v][u] = 0
+
+def lv_graph():
+    level = [-1]*(tt+2)
+    level[0] = 0
+    bfs = deque([0])
+    while bfs:
+        now = bfs.popleft()
+        for next in graph[now]:
+            if level[next] == -1 and capa[now][next]-flow[now][next] > 0:
+                bfs.append(next)
+                level[next] = level[now]+1
+    return level
+
+def dinic(now,cut):
+    if now == tt+1: return cut
+    for n in range(work[now],len(graph[now])):
+        next = graph[now][n]
+        resid = capa[now][next]-flow[now][next]
+        if level[now]+1 == level[next] and resid > 0:
+            f = dinic(next,min(cut,resid))
+            if f > 0:
+                flow[now][next] += f
+                flow[next][now] -= f
+                return f
+        work[now] += 1
+    return 0
+
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    tt = n*100
+    i,g,s = map(int,input().split())
+    m = int(input())
+    graph = [[] for _ in range(tt+2)]
+    capa = [{} for _ in range(tt+2)]
+    flow = [{} for _ in range(tt+2)]
+
+    for u in range(1,n+1):
+        for v in range(-99,-99+s):
+            connect(u*100+v,u*100+v+1)
+    connect(0,i*100-99)
+
+    for _ in range(m):
+        hos = int(input())
+        if hos == i:
+            connect(0,tt+1)
+        else:
+            for k in range(-99,-99+s):
+                connect(hos*100+k,tt+1)
+
+    for _ in range(int(input())):
+        a,b,p,t = map(int,input().split())
+        if a == i:
+            connect(0,b*100-100+t,p)
+        l = min(-99+s,-t+1)
+        for k in range(-99,l):
+            connect(a*100+k,b*100+k+t,p)
+
+    arrive = 0
+    while True:
+        level = lv_graph()
+        if level[-1] == -1: break
+
+        work = [0]*(tt+2)
+        while True:
+            f = dinic(0,int(1e9))
+            if f == 0: break
+            arrive += f
+            if arrive >= g: break
+        if arrive >= g: break
+
+    print(min(g,arrive))
