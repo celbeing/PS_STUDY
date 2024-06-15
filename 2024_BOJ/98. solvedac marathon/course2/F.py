20860cd9b554674b025415b3148ac138b5ffff63
전라남도교육지원청 <kimsd1983@naver.com>
2024-06-15 20:55:43 +0900

    24.06.15.

---
+++ 2024_BOJ/98. solvedac marathon/course2/F.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+dist = dict()
+dist["Seoul"] = 0.0
+dist["Yeongdeungpo"] = 9.1
+dist["Anyang"] = 23.9
+dist["Suwon"] = 41.5
+dist["Osan"] = 56.5
+dist["Seojeongri"] = 66.5
+dist["Pyeongtaek"] = 75.0
+dist["Seonghwan"] = 84.4
+dist["Cheonan"] = 96.6
+dist["Sojeongni"] = 107.4
+dist["Jochiwon"] = 129.3
