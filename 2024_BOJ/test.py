4fad7069a4c5c7f2153d767e2e51e2919fe84c8b
전라남도교육지원청 <kimsd1983@naver.com>
2024-06-17 19:33:02 +0900

    24.06.17.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,1 +1,20 @@
-print(-1%4)
+import sys
+input = sys.stdin.readline
+N,M,A = map(int,input().split())
+score = list(map(int,input().split()))
+score.sort()
+score.append(1e9)
+hands = [score[i] for i in range(N)]
+hands.append(-1e9)
+index = [i for i in range(N)]
+
+for m in range(M):
+	for i in range(N):
+		while index[i] < N and hands[i] >= score[index[i]+1]:
+			index[i] += 1
+		hands[i] += score[index[i]]
+
+for i in range(N):
+    if hands[i] - score[i] >= A:
+        print(score[i])
+        exit()
