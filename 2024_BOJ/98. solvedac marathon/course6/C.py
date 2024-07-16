15a0b7dc53c7a0437899fbcadaad10e9747fdbfb
전라남도교육지원청 <kimsd1983@naver.com>
2024-07-16 09:17:06 +0900

    24.07.16.

--- 2024_BOJ/98. solvedac marathon/course6/C.py
+++ 2024_BOJ/98. solvedac marathon/course6/C.py
@@ -3,6 +3,16 @@ input = sys.stdin.readline
 while True:
     N = int(input())
     if N == 0: break
-    P = [int(input()) for _ in range(N)]
-    high = 0
-    
+    dp = [0]*N
+    dp[0] = int(input())
+    high = -int(1e10)
+    if dp[0] > high: high = dp[0]
+    for i in range(1,N):
+        k = int(input())
+        if dp[i-1]+ k > k:
+            dp[i] = dp[i-1]+k
+        else:
+            dp[i] = k
+        if dp[i] > high:
+            high = dp[i]
+    print(high)
---
+++ 2024_BOJ/98. solvedac marathon/course6/E.py
@@ -0,0 +1,4 @@
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+R = [list(map(int,input().split())) for _ in range(K)]
+M = [list(map(int,input().split())) for _ in range(M)]
