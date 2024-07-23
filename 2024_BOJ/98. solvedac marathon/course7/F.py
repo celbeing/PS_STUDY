316241b3665767d146a308c9cef30e55ce2f656e
전라남도교육지원청 <kimsd1983@naver.com>
2024-07-23 14:55:05 +0900

    24.07.23.

--- 2024_BOJ/98. solvedac marathon/course7/F.py
+++ 2024_BOJ/98. solvedac marathon/course7/F.py
@@ -6,6 +6,27 @@ es = list(map(int,input().split()))
 c1 = []
 c2 = []
 
+def count(a,b):
+    p,q = 0,0
+    l,o = 0,0
+    for i in range(N):
+        if a[i]: l += 1
+        else: o += 1
+        if b[i]: l -= 1
+        else: o -= 1
+    ret = 0
+    if l == o == 0:
+        while p<N and q<N:
+            while p<N and a[p] == 0: p += 1
+            while q<N and b[q] == 0: q += 1
+            if p<N and q<N:
+                ret += abs(p-q)
+                p += 1
+                q += 1
+        return ret
+    else:
+        return 99
+
 for i in range(M):
     if i&1:
         c1 += [0]*es[i]
@@ -14,5 +35,4 @@ for i in range(M):
         c1 += [1]*es[i]
         c2 += [0]*es[i]
 
-# 맨 앞 비트부터 세어보기
-# 맨 뒤 비트부터 세어보기
+print(min(count(ss,c1),count(ss,c2)))
