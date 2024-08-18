52d9e1da911caadec6a6dbaa54009392aea02564
전라남도교육지원청 <kimsd1983@naver.com>
2024-08-18 22:43:09 +0900

    24.08.18.

--- 2024_BOJ/98. solvedac marathon/course11/H.py
+++ 2024_BOJ/98. solvedac marathon/course11/H.py
@@ -11,12 +11,35 @@ def simulate(rabbits):
     for i in range(N):
         if rabbits & 1<<i:
             rabbit[n][0] = N-i-1
-            for j in range(N-3,-1,-1):
-
+            for j in range(N-2):
+                if rabbit[n][j] == 0: rabbit[n][j+1] = 1
+                elif rabbit[n][j] >= N-2-j: rabbit[n][j+1] = rabbit[n][j]-1
+                elif board[rabbit[n][j]] == "W": rabbit[n][j+1] = rabbit[n][j]-1
+                elif board[rabbit[n][j]] == "B": rabbit[n][j+1] = rabbit[n][j]+1
+                else:
+                    if j > 0: rabbit[n][j+1] = rabbit[n][j-1]
+                    else: rabbit[n][j+1] = rabbit[n][j]-1
             n += 1
+    for i in range(1,N-1):
+        for j in range(n):
+            if rabbit[j][i] < 0: continue
+            flag = False
+            for k in range(j+1,n):
+                if rabbit[j][i] == rabbit[k][i]:
+                    flag = True
+                    for t in range(i,N-1):
+                        rabbit[k][t] = -1
+                if flag:
+                    for t in range(i,N-1):
+                        rabbit[j][t] = -1
+    ret = 0
+    for i in range(r):
+        if 0<=rabbit[i][-1]<=1: ret += 1
+    return ret
 
 def bt(d, i, m):
-    if d == r: return simulate(m)
+    if d == r:
+        return simulate(m)
     ret = 0
     b = 1 << (N-i)
     for j in range(N-i-r+d):
