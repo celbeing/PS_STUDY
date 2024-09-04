86ba03988cb9ec29524810aa6f369c3eaa17c55f
전라남도교육지원청 <kimsd1983@naver.com>
2024-09-04 23:30:00 +0900

    24.09.04.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,12 +1,22 @@
 import sys
 input = sys.stdin.readline
-N,H = map(int,input().split())
-dp = [0]*(H+1)
-dp[0] = 1
-A = list(map(int,input().split()))
-for i in range(1,H+1):
-    for a in A:
-        if a > i: continue
-        dp[i] += dp[i-a]
-    dp[i] %= 1000000007
-print(dp[-1])
+
+def sol():
+    N = int(input())
+    i, j = 0, 0
+    s = 0
+    res = 0
+    while j < N:
+        if s < N:
+            j += 1
+            s += j
+        elif s > N:
+            s -= i
+            i += 1
+        else:
+            res += 1
+            j += 1
+            s += j
+    print(res)
+
+sol()
