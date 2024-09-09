225916d276cd61e5d200c7aafc15d93234afffea
전라남도교육지원청 <kimsd1983@naver.com>
2024-09-09 19:08:53 +0900

    24.09.09.

---
+++ 2024_BOJ/99. homework/1019.py
@@ -0,0 +1,18 @@
+#1019: 책 페이지
+def solution():
+    N = input()
+    result = [0] * 10
+    digit = len(N) - 1
+    for n in N:
+        for i in range(int(n)):
+            result[i] += 10**digit
+            for j in range(10):
+                if digit >= 1: result[j] += digit * (10**(digit - 1))
+        result[0] -= 10**digit
+        if digit:
+            result[int(n)] += int(''.join(N[-digit:])) + 1
+        else:
+            result[int(n)] += 1
+        digit -= 1
+    print(*result)
+solution()
--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -2,12 +2,9 @@ import sys
 input = sys.stdin.readline
 
 def sol():
-    N = int(input())
-    res = []
-    for i in range(1,N+1):
-        res.append(i)
-        if i % 6 == 0:
-            res.append("Go!")
-    if N % 6: res.append("Go!")
-    print(*res)
+    n = int(input())
+    res = 0
+    for _ in range(n):
+        res += int(input())
+    print(res)
 sol()
