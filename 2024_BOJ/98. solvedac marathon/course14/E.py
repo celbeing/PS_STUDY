a5214dd3e0f2868fcef8da196cbecffe673a4c54
전라남도교육지원청 <kimsd1983@naver.com>
2024-09-10 22:37:33 +0900

    24.09.10.

--- 2024_BOJ/98. solvedac marathon/course14/E.py
+++ 2024_BOJ/98. solvedac marathon/course14/E.py
@@ -3,4 +3,11 @@ import sys
 input = sys.stdin.readline
 def solution():
     N, K = map(int, input().split())
-    
+    alpha = list("a b c d e f g h i j k l m n o p q r s t u v w x y z".split())
+    res = ""
+    for i in range(N-K):
+        res += "a"
+    for i in range(K):
+        res += alpha[i]
+    print(res)
+solution()
