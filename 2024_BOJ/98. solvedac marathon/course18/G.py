83e04cbbfac6514a775d27de6219320bb173b8a3
전라남도교육지원청 <kimsd1983@naver.com>
2024-10-05 14:55:24 +0900

    24.10.05.

--- 2024_BOJ/98. solvedac marathon/course18/G.py
+++ 2024_BOJ/98. solvedac marathon/course18/G.py
@@ -5,39 +5,39 @@ input = sys.stdin.readline
 def solution():
     d = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
     N, M, K = map(int, input().split())
-    stu = dict()
+    stu = [[0] * (M + 1) for _ in range(N + 1)]
     cor = dict()
     for i in range(1, K + 1):
         X, Y = map(int, input().split())
-        stu[X * 1000 + Y] = i
+        stu[X][Y] = i
         cor[i] = (X, Y)
     S = int(input())
-    link = [-1] * (K + 1)
+    link = [0] * (K + 1)
     paper = [0] * (K + 1)
-    link[S] = 0
+    link[S] = -1
+    link[0] = -1
     bfs = deque([S])
     new = []
     while bfs:
         now = bfs.popleft()
-        x, y = cor[now]
-        for de in d:
-            dx = x + de[0]
-            dy = y + de[1]
-            next = dx * 1000 + dy
-            if next in stu and link[stu[next]] == -1:
-                new.append(stu[next])
-                link[stu[next]] = now
+        for i in range(8):
+            dx, dy = cor[now]
+            dx += d[i][0]
+            dy += d[i][1]
+            if 0 < dx <= N and 0 < dy <= M and link[stu[dx][dy]] == 0:
+                new.append(stu[dx][dy])
+                link[stu[dx][dy]] = now
         if not bfs:
-            new.sort(reverse=True)
-            while new: bfs.append(new.pop())
+            new.sort()
+            bfs += new
+            new.clear()
     for i in range(1, K + 1):
         k = i
-        if link[k] == -1:
+        if link[k] == 0:
             print(-1)
             return
-        while k > 0:
+        while k >= 0:
             paper[k] += 1
             k = link[k]
     print(*paper[1:])
 solution()
-#재귀로 상향 탐색해야 될 것 같음
--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,24 +1,6 @@
 import sys
 input = sys.stdin.readline
 def solution():
-    N = int(input())
-    A, B = map(int, input().split())
-    sat = [tuple(map(int, input().split())) for _ in range(N)]
-    sat.reverse()
-    dp = [[[[0] * 2 for _ in range(101)] for _ in range(101)] for _ in range(101)]
-    for n in range(1, N + 1):
-        p, q, r, s = sat.pop()
-        t = max(p, q)
-        for b in range(n + 1):
-            for a in range(n - b + 1):
-                study, rest = 0, 0
-                if a: rest = max(dp[n - 1][a - 1][b]) + s
-                if b: study = max(dp[n - 1][a][b - 1]) + t
-                dp[n][a][b][0] = max(study, rest)
-                if a + b < n: dp[n][a][b][1] = dp[n - 1][a][b][0] + r
-    high = 0
-    for a in range(A + 1):
-        for b in range(B, N + 1):
-            high = max(high, max(dp[N][a][b]))
-    print(high)
+    l = [int(input()) for _ in range(4)]
+    print((l[1]+l[3]+max(l[0],l[2]))*2+4)
 solution()
