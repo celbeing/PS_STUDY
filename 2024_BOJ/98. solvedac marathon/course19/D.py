aaacbfab2587d68fb92e9beab003b904bbd52e4a
전라남도교육지원청 <kimsd1983@naver.com>
2024-10-14 00:14:04 +0900

    24.10.13.

--- 2024_BOJ/98. solvedac marathon/course19/D.py
+++ 2024_BOJ/98. solvedac marathon/course19/D.py
@@ -1,4 +1,33 @@
 #26286: Football
 import sys
 input = sys.stdin.readline
+def score(a, b, c = 1):
+    for _ in range(c):
+        print(f"{a}:{b}")
 def solution():
+    n = int(input())
+    a = int(input())
+    b = int(input())
+    if a + b <= n:
+        print(n - a - b)
+        score(0, 0, n - a - b)
+        score(1, 0, a)
+        score(0, 1, b)
+    elif n == 1:
+        if a == b:
+            print(1)
+        else:
+            print(0)
+        score(a, b)
+    elif n < a + b:
+        print(0)
+        if a < n:
+            score(1, 0, a)
+            score(0, 1, n - a - 1)
+            score(0, b - n + a + 1)
+        elif b < n:
+            score(0, 1, b)
+            score(1, 0, n - b - 1)
+            score(a - n + b + 1, 0)
+        else:
+            
