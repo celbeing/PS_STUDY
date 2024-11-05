e28d9bae32dfdaecbc4f333cef51183bfb2fdcb3
전라남도교육지원청 <kimsd1983@naver.com>
2024-11-05 23:42:24 +0900

    24.11.05.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -2,21 +2,8 @@ import sys, random
 from collections import deque
 input = sys.stdin.readline
 def solution():
-<<<<<<< Updated upstream
-    n = input().strip()
-    res = "1"
-    k = 2
-    while int(res) < int(n):
-        res += str(k)
-        k += 1
-    if res == n:
-        print(k - 1)
-    else: print(-1)
-=======
-    n = int(input())
-    ori = deque([i for i in range(1, n + 1)])
-    random.random()
-    random.shuffle(ori)
-    print(ori)
->>>>>>> Stashed changes
+    a = 1
+    b = 2
+    a, b = b, a
+    print(a, b)
 solution()
