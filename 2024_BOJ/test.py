c3854723c72388711062cd0b216c839e086b8026
전라남도교육지원청 <kimsd1983@naver.com>
2024-11-10 10:48:59 +0900

    24.11.10.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,9 +1,14 @@
-import sys, random
-from collections import deque
+import sys
 input = sys.stdin.readline
-def solution():
-    random.random()
-    num = [i for i in range(1, int(input()) + 1)]
-    random.shuffle(num)
-    print(*num)
-solution()
+def sol(arr, d):
+    if d == 6:
+        print(arr)
+        return 1
+    ret = 0
+    for i in range(5):
+        if i in arr: continue
+        elif i == 0 and d in {1, 3, 5}: continue
+        elif i == 1 and d == 3: continue
+        ret += sol(arr+[i], d + 1)
+    return ret
+print(sol([],1))
