dbb9da7b7e278764ecc071b9f2849c227f131953
전라남도교육지원청 <kimsd1983@naver.com>
2024-11-12 17:49:43 +0900

    24.11.12.

--- 2024_BOJ/98. solvedac marathon/course23/D.py
+++ 2024_BOJ/98. solvedac marathon/course23/D.py
@@ -4,4 +4,16 @@ input = sys.stdin.readline
 def solution():
     N = int(input())
     arr = list(map(int, input().split()))
-    ldp, rdp = [0] * 
+    dp = [0] * N
+    dp2 = [0] * N
+    for k in range(1, N):
+        i = N - k - 1
+        for j in range(N - 1, k - 1, -1):
+            dp2[j] = dp[j]
+            if arr[i] == arr[j]:
+                dp[j] = dp2[j - 1]
+            else:
+                dp[j] = min(dp[j], dp[j - 1]) + 1
+            i -= 1
+    print(dp[-1])
+solution()
