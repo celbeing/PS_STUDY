ad03bfb10e52c2562c885bcd1bca47a92431f417
전라남도교육지원청 <kimsd1983@naver.com>
2024-11-17 21:23:46 +0900

    24.11.17.

--- 2024_BOJ/1. Implementation/5. Recursion/18511.py
+++ 2024_BOJ/1. Implementation/5. Recursion/18511.py
@@ -1,7 +1,15 @@
 #18511: 큰 수 구성하기
 import sys
 input = sys.stdin.readline
+miis = lambda: map(int, input().split())
 def solution():
-    N, K = map(int, input().split())
-    num = sorted(list(map(int, input().split())))
-    
+    N, K = miis()
+    num = sorted(list(set(miis())))
+    def dfs(n):
+        ret = n
+        if n > N: return 0
+        for m in num:
+            ret = max(ret, dfs(n * 10 + m))
+        return ret
+    print(dfs(0))
+solution()
