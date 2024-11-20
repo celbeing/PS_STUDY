040d05dfb5fdd4ab9e9de00b0ced206d21cc4925
전라남도교육지원청 <kimsd1983@naver.com>
2024-11-20 13:58:33 +0900

    24.11.20.

--- 2024_BOJ/97. Blobnom/24년 11월 3주차/27946.py
+++ 2024_BOJ/97. Blobnom/24년 11월 3주차/27946.py
@@ -1,24 +1,41 @@
 # 27945: 슬슬 가지를 먹지 않으면 죽는다
 import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
-def solution:
+def solution():
     n, m = map(int, input().split())
-    link = [tuple(map(int, input().split())) for _ in range(m)]
-    link.sort(key = lambda: x[2])
+    date = [dict() for _ in range(n + 1)]
+    link = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v, t = map(int, input().split())
+        date[u][v] = t
+        date[v][u] = t
+        link[u].append(v)
+        link[v].append(u)
+
     head = [i for i in range(n + 1)]
     
     def union(a, b):
         A = find(a)
         B = find(b)
-        if A <= B:
-            pass
+        if A == B:
+            return False
+        elif A < B:
+            head[b] = A
         else:
-            pass
-        return 0
+            head[a] = B
+        return True
 
     def find(k):
         while head[k] != k:
             k = head[k]
         return k
-    k = 1
-    
+
+    near = []
+    heappush(near, (0, 1))
+    for i in range(1, n):
+        k, now = heappop(near)
+        for 
+
+
+solution()
