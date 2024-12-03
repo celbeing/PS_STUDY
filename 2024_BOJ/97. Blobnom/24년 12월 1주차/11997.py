8c78afbb0a3f3415a36412d93103cd1158e5a4c3
전라남도교육지원청 <kimsd1983@naver.com>
2024-12-04 08:48:35 +0900

    24.12.02.

--- 2024_BOJ/97. Blobnom/24년 12월 1주차/11997.py
+++ 2024_BOJ/97. Blobnom/24년 12월 1주차/11997.py
@@ -2,4 +2,42 @@
 import sys
 input = sys.stdin.readline
 def solution():
-    N = int(input())
+    n = int(input())
+    cow = [[0] * 1001 for _ in range(1001)]
+    cor = []
+    cor_x = set()
+    cor_y = set()
+    for _ in range(n):
+        x, y = map(int, input().split())
+        cor_x.add(x)
+        cor_y.add(y)
+        cor.append((x, y))
+    cor_x = sorted(list(cor_x))
+    cor_y = sorted(list(cor_y))
+    corzip_x = dict()
+    corzip_y = dict()
+    k = 1
+    for X in cor_x:
+        corzip_x[X] = k
+        k += 1
+    k = 1
+    for Y in cor_y:
+        corzip_y[Y] = k
+        k += 1
+    for x, y in cor:
+        cow[corzip_x[x]][corzip_y[y]] += 1
+    for i in range(1001):
+        for j in range(1, 1001):
+            cow[i][j] += cow[i][j - 1]
+    for i in range(1001):
+        for j in range(1, 1001):
+            cow[j][i] += cow[j - 1][i]
+    result = 1000
+    for i in range(1001):
+        for j in range(1001):
+            h = cow[-1][j] - cow[i][j]
+            v = cow[i][-1] - cow[i][j]
+            o = n - h - v - cow[i][j]
+            result = min(result, max(h, v, o, cow[i][j]))
+    print(result)
+solution()
