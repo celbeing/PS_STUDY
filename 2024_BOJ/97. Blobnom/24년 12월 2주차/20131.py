6f06ac2e1c4952254d54c41b26a66161b715e5d9
전라남도교육지원청 <kimsd1983@naver.com>
2024-12-13 14:53:41 +0900

    24.12.12.

--- 2024_BOJ/97. Blobnom/24년 12월 2주차/20131.py
+++ 2024_BOJ/97. Blobnom/24년 12월 2주차/20131.py
@@ -1,6 +1,7 @@
 # 20131: 트리 만들기
 import sys
 from collections import deque
+from heapq import heappush, heappop
 input = sys.stdin.readline
 def solution():
     n = int(input())
@@ -8,33 +9,22 @@ def solution():
     P = deque(list(map(int, input().split())))
     for p in P: count[p] += 1
     tree = []
-    head = [i for i in range(n + 1)]
-
-    def find(k):
-        while k != head[k]:
-            k = head[k]
-        return k
-
-    def union(a, b):
-        B = find(b)
-        head[a] = B
-        return 0
+    L = []
+    for i in range(1, n + 1):
+        if count[i] == 0: heappush(L, -i)
 
     for _ in range(n - 2):
-        k = find(n)
-        while count[k]:
-            k -= 1
-            k = find(k)
-        if k < P[0]:
-            tree.append((k, P[0]))
+        l = -heappop(L)
+        if P[0] > l:
+            tree.append((l, P[0]))
         else:
-            tree.append((P[0], k))
-        count[P.popleft()] -= 1
-        union(k, k - 1)
-    a = find(n)
-    union(a, a - 1)
-    b = find(n)
-    tree.append((b, a))
+            tree.append((P[0], l))
+        count[P[0]] -= 1
+        if count[P[0]] == 0:
+            heappush(L, -P[0])
+        P.popleft()
+
+    tree.append((-L[1], -L[0]))
     tree.sort()
     for e in tree:
         print(*e)
