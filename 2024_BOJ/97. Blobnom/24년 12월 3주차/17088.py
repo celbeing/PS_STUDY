3fe67726711d5d2c4c4ec9e9eef6a11b0d9b8496
전라남도교육지원청 <kimsd1983@naver.com>
2024-12-17 11:39:20 +0900

    24.12.17.

--- 2024_BOJ/97. Blobnom/24년 12월 3주차/17088.py
+++ 2024_BOJ/97. Blobnom/24년 12월 3주차/17088.py
@@ -4,7 +4,24 @@ input = sys.stdin.readline
 def solution():
     n = int(input())
     b = list(map(int, input().split()))
-    d = [0] * 9
-    c = [-1] * 9
-    if b[1] - b[0] % 3 == 0:
-        
+    if n == 1:
+        print(0)
+        return 0
+    D = b[1] - b[0]
+    S = b[0]
+    result = n + 1
+    for i in range(-1, 2):
+        for j in range(-i - 1, -i + 2):
+            d = D + j
+            count = 0
+            now = S + i
+            for c in b:
+                k = abs(now - c)
+                if k == 1: count += 1
+                elif k > 1: break
+                now += d
+            else:
+                result = min(result, count)
+    if result == n + 1: result = -1
+    print(result)
+solution()
---
+++ 2024_BOJ/97. Blobnom/24년 12월 3주차/27297.py
@@ -0,0 +1,7 @@
+# 27297: 맨해튼에서의 모임
+import sys
+input = sys.stdin.readline
+def solution():
+    d, n = map(int, input().split())
+    # 평균으로 구하는 거 아니네?
+solution()
