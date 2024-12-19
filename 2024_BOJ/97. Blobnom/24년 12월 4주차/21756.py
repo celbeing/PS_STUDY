6d5a6e9679b3e2ecceddc286a8ece8e8e5fc3240
전라남도교육지원청 <kimsd1983@naver.com>
2024-12-20 00:07:32 +0900

    24.12.19.

---
+++ 2024_BOJ/97. Blobnom/24년 12월 4주차/21756.py
@@ -0,0 +1,3 @@
+# 21756: 지우개
+import math
+print(2 ** math.floor(math.log2(int(input()))))
---
+++ 2024_BOJ/97. Blobnom/24년 12월 4주차/25589.py
@@ -0,0 +1,6 @@
+# 25589: 푸앙이와 코인
+import sys
+input = sys.stdin.readline
+def solution():
+    n = int(input())
+    
