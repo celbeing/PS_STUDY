8ef4146bde57a8f944b6febdd920e82552df8ee3
전라남도교육지원청 <kimsd1983@naver.com>
2024-12-24 00:55:51 +0900

    24.12.23.

---
+++ 2024_BOJ/97. Blobnom/24년 12월 4주차/32189.py
@@ -0,0 +1,10 @@
+# 32189: '한국디지털미디어고등학교'는 너무 길다.
+import sys
+input = sys.stdin.readline
+def solution():
+    s = list(input().strip())
+    def lcs(a, b):
+        k = 0
+        return k
+    print()
+solution()
