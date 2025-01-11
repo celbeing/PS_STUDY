0d231f03e190f231d6691e329a31a36c2a80d02d
전라남도교육지원청 <kimsd1983@naver.com>
2025-01-12 00:51:50 +0900

    25.01.11.

--- 2024_BOJ/97. Blobnom/25년 1월 2주차/32631.py
+++ 2024_BOJ/97. Blobnom/25년 1월 2주차/32631.py
@@ -3,7 +3,20 @@ import sys
 input = sys.stdin.readline
 def solution():
     n, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    
+    a = [0] + list(map(int, input().split()))
+    b = [0] + list(map(int, input().split()))
+    for i in range(1, n + 1):
+        a[i] += a[i - 1]
+        b[i] += b[i - 1]
+    res = max(a[-1], b[-1])
+    A = [a[-1]] * (k + 1)
+    B = [b[-1]] * (k + 1)
+    for i in range(n - k, n + 1):
+        for j in range(n - i):
+            A[i - n + k] = min(A[i - n + k], a[i + j] - a[j])
+            B[i - n + k] = min(B[i - n + k], b[i + j] - b[j])
+    for i in range(k + 1):
+        for j in range(k - i, k + 1):
+            res = min(res, max(A[i], B[j]))
+    print(res)
 solution()
--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,4 +1,27 @@
+import sys
+input = sys.stdin.readline
 def solution():
-   s = input()
-
+    n, k = map(int, input().split())
+    a = [0] + list(map(int, input().split()))
+    b = [0] + list(map(int, input().split()))
+    for i in range(1, n + 1):
+        a[i] += a[i - 1]
+        b[i] += b[i - 1]
+    inf = max(a[-1], b[-1])
+    a_dp = [a[-1]] * (n + 1)
+    b_dp = [b[-1]] * (n + 1)
+    a_dp[0] = 0
+    b_dp[0] = 0
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            ta = a[j] - a[i - 1]
+            tb = b[j] - b[i - 1]
+            a_dp[j - i + 1] = min(a_dp[j - i + 1], ta)
+            b_dp[j - i + 1] = min(b_dp[j - i + 1], tb)
+    res = inf
+    for i in range(n + 1):
+        j = 2 * n - k - i
+        if 0 <= j <= n:
+            res = min(res, max(a_dp[i], b_dp[j]))
+    print(res)
 solution()
