ae0c9d2a225bdc6889e3ee073eb3e67153f90ef6
전라남도교육지원청 <kimsd1983@naver.com>
2025-01-28 00:53:32 +0900

    25.01.28.

--- 2024_BOJ/97. Blobnom/25년 2월 1주차/20054.py
+++ 2024_BOJ/97. Blobnom/25년 2월 1주차/20054.py
@@ -4,5 +4,13 @@ input = sys.stdin.readline
 def solution():
     n, k = map(int, input().split())
     mod = int(1e9) + 7
-
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
+    dp[0][0] = 1
+    for i in range(1, k + 1):
+        dp[i][0] = 1
+        for j in range(1, min(n + 1, 1 << i)):
+            for t in range(j):
+                dp[i][j] += dp[i - 1][t] * dp[i - 1][j - t - 1]
+                dp[i][j] %= mod
+    print(dp[k][n])
 solution()
