ad228a8c7d39de81c9fdc43f6db8c80503ba075d
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-02-13 13:13:12 +0900

    4883.py 업데이트

--- 2024_BOJ/99. homework/4883.py
+++ 2024_BOJ/99. homework/4883.py
@@ -5,13 +5,15 @@ def solution():
     while True:
         tc += 1
         n = int(input())
-        cost = list(map(int, input().split()))
-        cost[0] = int(1e9)
+        cost = [[],[]]
+        cost[0] = list(map(int, input().split()))
+        cost[0][0] = int(1e9)
         for _ in range(n - 1):
-            now = list(map(int, input().split()))
-            now[0] += min(cost[:2])
-            now[1] += min(cost)
-            now[2] += min(cost[1:])
-            cost = now[:]
-        print(f"{tc}. {cost[1]}")
+            cost[1] = list(map(int, input().split()))
+            now = [int(1e9)] * 3
+            now[0] = min(cost[:2]) + cost[1][0]
+            now[1] = min(min(cost), now[0]) + cost[1][1]
+            now[2] = min(min(cost[1:]), now[1]) + cost[1][2]
+            cost[0] = now[:]
+        print(f"{tc}. {cost[0][1]}")
 solution()
