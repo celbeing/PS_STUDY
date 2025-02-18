2ea78c1ba9851c6d4d9995a91dba58e9ec27c54d
전라남도교육지원청 <kimsd1983@naver.com>
2025-02-18 14:05:54 +0900

    25.02.18.

--- 2024_BOJ/97. Blobnom/25년 2월/2551.py
+++ 2024_BOJ/97. Blobnom/25년 2월/2551.py
@@ -1,7 +1,12 @@
 # 2551: 두 대표 자연수
 import sys
-from math import round
 input = sys.stdin.readline
-n = int(input())
-a = sorted(list(map(int, input().split())))
-print(a[(n - 1) >> 1], round(sum(a) / n))
+def solution():
+    n = int(input())
+    a = sorted(list(map(int, input().split())))
+    x = a[(n - 1) >> 1]
+    y = round(sum(a) / n)
+    if y ** 2 - sum(a) * y * 2 / n >= (y - 1) ** 2 - sum(a) * (y - 1) * 2 / n:
+        y -= 1
+    print(x, y)
+solution()
