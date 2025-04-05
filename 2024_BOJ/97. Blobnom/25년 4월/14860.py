296bade5b32993d27f528972e32f721e2fca023b
전라남도교육지원청 <kimsd1983@naver.com>
2025-04-05 09:25:11 +0900

    25.04.25.

--- 2024_BOJ/97. Blobnom/25년 4월/14860.py
+++ 2024_BOJ/97. Blobnom/25년 4월/14860.py
@@ -3,5 +3,39 @@ import sys
 input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split())
-    return 0
+    if n > m:
+        t = n
+        n = m
+        m = t
+    res = 1
+    mod = int(1e9 + 7)
+
+    def fast_mult(n, k):
+        if k == 0:
+            return 1
+
+        x = fast_mult(n, k // 2)
+
+        if k & 1:
+            return x * x * n % mod
+        else:
+            return x * x % mod
+
+    sieve = [1] * (m + 1)
+    prime = []
+    for i in range(2, m + 1):
+        if sieve[i]:
+            prime.append(i)
+            for j in range(i * i, m + 1, i):
+                sieve[j] = 0
+
+    for p in prime:
+        c = 0
+        k = p
+        while k <= n:
+            c += (n // k) * (m // k)
+            k *= p
+        res *= fast_mult(p, c)
+        res %= mod
+    print(res)
 solution()
--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,16 +1,12 @@
 import sys
 input = sys.stdin.readline
 def solution():
-    n = int(input())
-    x, y = map(int, input().split())
-    if n == 1:
-        print(0)
-        return
-    if (x == 1 or x == n) and (y == 1 or y == n):
-        print(2)
-    elif (x == 1 or x == n) or (y == 1 or y == n):
-        print(3)
-    else:
-        print(4)
-    return
+    rec = list(input().strip())
+    a, b = 0, 0
+    for i in range(0, len(rec), 2):
+        if rec[i] == 'A':
+            a += int(rec[i + 1])
+        else:
+            b += int(rec[i + 1])
+    print('A' if a > b else 'B')
 solution()
