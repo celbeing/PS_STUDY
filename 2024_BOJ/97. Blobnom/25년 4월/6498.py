b2bd85687e346f504a2e0606bfcff8a0bb872b72
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-04-12 00:36:05 +0900

    25.04.12.

--- 2024_BOJ/97. Blobnom/25년 4월/6498.py
+++ 2024_BOJ/97. Blobnom/25년 4월/6498.py
@@ -8,8 +8,26 @@ def solution():
         if n == 0: break
         dp = [[inf] * (n + 1) for _ in range(n + 1)]
         dp[0][0] = 0
-
         c = [0] + list(map(int, input().split()))
+        s = [0] * (n + 1)
         for i in range(1, n + 1):
-            pass
-        # 이게 왜 골드 4임..?
+            s[i] = s[i - 1] + c[i]
+        opt = [[0] * (n + 1) for _ in range(n + 1)]
+        for j in range(1, n + 1):
+            opt[n][j] = n  # 경계 처리
+            for i in range(n, j - 1, -1):
+                l = opt[i][j - 1]
+                r = opt[i + 1][j] if i + 1 <= n else n
+                dp[i][j] = inf
+                for k in range(l, r + 1):
+                    t = dp[k][j - 1] + (s[i] - s[k]) * (i + j)
+                    if dp[i][j] > t:
+                        dp[i][j] = t
+                        opt[i][j] = k
+
+        res = dp[n][1]
+        for j in range(2, n + 1):
+            if res > dp[n][j]:
+                res = dp[n][j]
+        print(res)
+solution()
