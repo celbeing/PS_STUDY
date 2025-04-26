ec9a8cbdaac1f1e8c4ad405f113d30ab49b90f84
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-04-27 08:46:43 +0900

    25.04.26.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,9 +1,19 @@
-n = int(input())
-count = 0
-while n > 1:
-  if n % 2 == 0:
-    n = n // 2
+for _ in range(int(input())):
+  card = int(input())
+  res = 0
+  for _ in range(8):
+    res += card % 10
+    card //= 10
+    k = card % 10
+    card //= 10
+    k *= 2
+    if k >= 10:
+      res += k % 10
+      k //= 10
+      res += k
+    else:
+      res += k
+  if res % 10:
+    print('F')
   else:
-    n = n * 3 + 1
-  count = count + 1
-print(count)
+    print('T')
