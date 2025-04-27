203bb364904438b65d92af7351bbc4d015ae4cde
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-04-28 07:42:31 +0900

    25.04.28.

--- 2024_BOJ/test.py
+++ 2024_BOJ/test.py
@@ -1,1 +1,10 @@
-print(15^4)
+path = "C:\\Users\\kimsd\\Desktop\\"
+file_no = 1
+while True:
+    i = input()
+    file = open(path + f"{file_no}.in", 'w')
+    w = file.writelines(f'{i}')
+    p, q = map(str, i.split())
+    file = open(path + f"{file_no}.out", 'w')
+    w = file.writelines(f'{p}\n{q}')
+    file_no += 1
