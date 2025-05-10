af4047673f5060c29a8ea4f8a0a875fa58903a08
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-05-10 13:32:31 +0900

    25.05.10.

--- 2024_BOJ/4. Sorting/20141.py
+++ 2024_BOJ/4. Sorting/20141.py
@@ -1,6 +1,7 @@
 # 20141: Vista 4
 import sys
 input = sys.stdin.readline
+<<<<<<< Updated upstream
 def solution():
     f = open(r"C:\Users\kimsd\Downloads\4.in", "r")
     n = int(f.readline())
@@ -14,4 +15,17 @@ def solution():
         res.append(i)
     res.append(dots[0][2])
     print(*res)
+=======
+f = open(r"C:\Users\kimsd\Downloads\4.in", "r")
+def solution():
+    n = int(f.readline().strip())
+    dots = []
+    for i in range(1, n + 1):
+        x, y = map(int, f.readline().strip().split())
+        dots.append((x, y, i))
+        dots.sort()
+    for x, y, i in dots:
+        print(i)
+    print(dots[0][2])
+>>>>>>> Stashed changes
 solution()
