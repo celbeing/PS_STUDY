995b3611d8dd66d772b5d3463f65ca0023c24676
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-05-12 00:16:53 +0900

    25.05.12.

--- 2024_BOJ/test_case_gen.py
+++ 2024_BOJ/test_case_gen.py
@@ -1,11 +1,15 @@
 import random, math
 random.random()
 
-path = r"C:\Users\kimsd\OneDrive\바탕 화면\tc\\"
+path = r"C:\Users\kimsd\Desktop\tc\\"
 
-for i in range(1, 51):
+for i in range(7, 51):
     file = open(path + f"{i}.in", "w+", encoding = 'utf-8')
-    S = input().strip()
-    w = file.writelines(f"{S}")
+    p, c, h = map(int, input().split())
+    w = file.writelines(f"{p}\n{c}\n{h}")
     file = open(path + f"{i}.out", "w+", encoding = 'utf-8')
-    w = file.writelines("{0:*^40}".format(S))
+    w = file.writelines("피자"*p)
+    w = file.writelines("\n")
+    w = file.writelines("치킨"*c)
+    w = file.writelines("\n")
+    w = file.writelines("햄버거"*h)
