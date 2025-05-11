135c31021559502917225274d40b634a20647c11
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-05-12 00:36:33 +0900

    25.05.11.

--- 2024_BOJ/1. Implementation/7. Ad-hoc/20142.py
+++ 2024_BOJ/1. Implementation/7. Ad-hoc/20142.py
@@ -10,8 +10,13 @@ def solution():
         dots.append((x, y, i))
     dots.sort()
     res = []
-    for x, y, i in dots:
-        res.append(i)
     res.append(dots[0][2])
+    for i in range(0, 100, 2):
+        for j in range(1, 100):
+            res.append(dots[i * 100 + j][2])
+        for j in range(99, 0, -1):
+            res.append(dots[(i + 1) * 100 + j][2])
+    for i in range(9900, -1, -100):
+        res.append(dots[i][2])
     print(*res)
 solution()
