340ace80d23ca725e71e33b0cf309b2df5a9d232
전라남도교육지원청 <kimsd1983@naver.com>
2025-05-17 00:41:12 +0900

    25.05.16.

--- 2024_BOJ/test_case_gen.py
+++ 2024_BOJ/test_case_gen.py
@@ -1,15 +1,15 @@
 import random, math
 random.random()
 
-path = r"C:\Users\kimsd\Desktop\tc\\"
+path = r"C:\Users\kimsd\OneDrive\바탕 화면\tc\\"
 
-for i in range(7, 51):
+for i in range(1, 51):
     file = open(path + f"{i}.in", "w+", encoding = 'utf-8')
-    p, c, h = map(int, input().split())
-    w = file.writelines(f"{p}\n{c}\n{h}")
+    ask = input().strip()
+    ans = input().strip()
+    w = file.writelines(f"{ask}\n{ans}")
     file = open(path + f"{i}.out", "w+", encoding = 'utf-8')
-    w = file.writelines("피자"*p)
-    w = file.writelines("\n")
-    w = file.writelines("치킨"*c)
-    w = file.writelines("\n")
-    w = file.writelines("햄버거"*h)
+    if ask == ans:
+        w = file.writelines("신원 확인 되었습니다.")
+    else:
+        w = file.writelines("누구냐!")
