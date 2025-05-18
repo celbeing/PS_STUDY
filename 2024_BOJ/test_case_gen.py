b835eeedffc06fdf8c2a65fde83381073377526e
전라남도교육지원청 <kimsd1983@naver.com>
2025-05-18 18:01:14 +0900

    25.05.18.

--- 2024_BOJ/test_case_gen.py
+++ 2024_BOJ/test_case_gen.py
@@ -1,15 +1,14 @@
 import random, math
 random.random()
 
-path = r"C:\Users\kimsd\Desktop\tc\\"
+path = r"C:\Users\kimsd\OneDrive\바탕 화면\tc\\"
 
-for i in range(1, 5):
+for i in range(1, 10):
     file = open(path + f"{i}.in", "w+", encoding = 'utf-8')
-    m = int(input())
-    p = int(input())
-    w = file.writelines(f"{m}\n{p}")
-    m += round(m * p / 100, 0)
-    m += round(m * p / 100, 0)
-    m += round(m * p / 100, 0)
+    rps = input().strip()
+    ym = input().strip()
+    w = file.writelines(f"{rps}\n{ym}")
+    sel = input().strip()
+    res = input().strip()
     file = open(path + f"{i}.out", "w+", encoding = 'utf-8')
-    w = file.writelines(f"{int(m)}")
+    w = file.writelines(f"{sel}\n{res}")
