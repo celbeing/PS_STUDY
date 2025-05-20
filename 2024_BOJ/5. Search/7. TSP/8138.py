2080b88d0a9956699fcc5e92e64548f1812b2c56
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-05-21 00:05:29 +0900

    25.05.20.

--- 2024_BOJ/5. Search/7. TSP/8138.py
+++ 2024_BOJ/5. Search/7. TSP/8138.py
@@ -13,8 +13,8 @@ def dijk(s):
     while hq:
         d, now = heappop(hq)
         if distance[now] < d: continue
-        for next in link[now]:
-            new_d = d + link[now][next]
+        for next, new_d in link[now]:
+            new_d += d
             if new_d < distance[next]:
                 distance[next] = new_d
                 heappush(hq, (new_d, next))
@@ -29,40 +29,48 @@ def dijk(s):
 def tsp(now, visit):
     if dp[now][visit] == 0:
         dp[now][visit] = inf
-        for next in range(1, k + 1):
+        for next in range(k):
             if visit & (1 << next): continue
             for check in cond[next]:
                 if visit & 1 << check == 0: break
             else:
-                dp[now][visit] = min(dp[now][visit], tsp(next, visit | (1 << next)) + dist[now][next])
+                dp[now][visit] = min(dp[now][visit], tsp(next, visit | (1 << next)) + dist[now + 1][next + 1])
     return dp[now][visit]
 
 # 0은 n번 지점
 dist = [[inf] * (k + 2) for _ in range(k + 2)]
-distance = [inf] * n
-link = [dict() for _ in range(n)]
-cond = [[] for _ in range(k + 1)]
+link = [[] for _ in range(n)]
+cond = [[] for _ in range(k)]
+from_start = [0] * k
 
 for _ in range(m):
     u, v, w = map(int, input().split())
     u -= 1; v -= 1
-    link[u][v] = w
-    link[v][u] = w
+    link[u].append((v, w))
+    link[v].append((u, w))
 
 for _ in range(int(input())):
     r, s = map(int, input().split())
-    r -= 1; s -= 1
+    r -= 2; s -= 2
     cond[s].append(r)
-for i in range(k + 1):
+for i in range(k):
     cond[i].sort()
 
 for i in range(k + 1):
     dijk(i)
     dist[-1][i] = dist[i][-1]
 
-dp = [[0] * (1 << (k + 1)) for _ in range(k + 1)]
-for i in range(1, k + 1):
-    dp[i][(1 << (k + 1)) - 1] = dist[i][-1]
+for i in range(k):
+    from_start[i] = dist[0][i + 1]
 
-res = tsp(0, 1)
+dp = [[0] * (1 << k) for _ in range(k)]
+for i in range(k):
+    dp[i][(1 << k) - 1] = dist[i + 1][-1]
+
+res = inf
+for i in range(k):
+    if cond[i]: continue
+    res = min(res, tsp(i, 1 << i) + from_start[i])
+if res == inf:
+    res = dist[0][-1]
 print(res)
