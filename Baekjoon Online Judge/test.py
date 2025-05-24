0b3a9a76b02e05274727e8af330e5631f4c956f7
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-05-25 01:26:33 +0900

    25.05.24.

--- Baekjoon Online Judge/test.py
+++ Baekjoon Online Judge/test.py
@@ -1,5 +1,11 @@
-n, m = map(int, input().split())
-for _ in range(n):
-    a, b = map(int, input().split())
-    m += b if a == 1 else -b
-print(m)
+recaman = set()
+n = 0
+num = 1
+while n < 1000:
+    if n - num <= 0 or n - num in recaman:
+        n += num
+    else:
+        n -= num
+    recaman.add(n)
+    print(f'{num}: {n}')
+    num += 1
--- Baekjoon Online Judge/test_case_gen.py
+++ Baekjoon Online Judge/test_case_gen.py
@@ -1,32 +1,19 @@
 import random, math
 random.random()
+inf = 1 << 31
 
-path = r"C:\Users\kimsd\OneDrive\바탕 화면\tc\\"
+path = r"C:\Users\kimsd\Desktop\tc\\"
 for i in range(1, 21):
-    n = random.randint(1, 10)
+    n = random.randint(1, 100000000)
     file = open(path + f"{i}.in", "w+", encoding = 'utf-8')
     w = file.writelines(f"{n}")
-    m = 1
-    l = []
-    for _ in range(n):
-        k = random.randint(1, 1000)
-        if k < 10:
-            k = 0
-        else:
-            k = random.randint(1, 10)
-        w = file.writelines(f"\n{k}")
-        l.append(k)
+
+
     file = open(path + f"{i}.out", "w+", encoding='utf-8')
-    for i in range(n - 1):
-        m *= l[i]
-        if m:
-            w = file.writelines(f"{m}\n")
-        else:
-            w = file.writelines(f"0이잖아")
-            break
-    else:
-        m *= l[-1]
-        if m:
-            w = file.writelines(f"{m}")
-        else:
-            w = file.writelines("0이잖아")
+    while n > 1:
+        if n % 2: n = n * 3 + 1
+        else: n >>= 1
+        if n == 1: break
+        if n >= inf: print('overflow')
+        w = file.writelines(f'{n}\n')
+    w = file.writelines(f'{n}')
