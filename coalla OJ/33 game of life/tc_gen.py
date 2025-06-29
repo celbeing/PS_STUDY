b13d1dd8fd88d18b223d49527177fbc77f7252db
전라남도교육지원청 <kimsd1983@naver.com>
2025-06-29 20:30:48 +0900

    25.06.29.

--- coalla OJ/33 game of life/tc_gen.py
+++ coalla OJ/33 game of life/tc_gen.py
@@ -4,18 +4,20 @@ n = 16
 
 path = r"C:\Users\kimsd\OneDrive\바탕 화면\tc\\"
 
-for tc in range(18, 21):
-    file = open(path + f'{tc}.in', 'w+', encoding='utf-8')
-    now = [0] * n
-    for i in range(n):
-        for j in range(n):
-            t = random.randint(1, 2)
-            if t == 1:
-                now[i] |= 1 << j
-        line = bin(now[i])[2:]
-        if i > 0:
-            w = file.writelines(f'\n')
-        w = file.writelines(f'{line:0>16}')
-    file = open(path + f'{tc}.out', 'w+', encoding='utf-8')
-    gen, period = solution(now)
-    w = file.writelines(f'{gen} {period}')
+for tc in range(31, 32):
+    while True:
+        file = open(path + f'{tc}.in', 'w+', encoding='utf-8')
+        now = [0] * n
+        for i in range(n):
+            for j in range(n):
+                t = random.randint(1, 6)
+                if t == 1:
+                    now[i] |= 1 << j
+            line = bin(now[i])[2:]
+            if i > 0:
+                w = file.writelines(f'\n')
+            w = file.writelines(f'{line:0>16}')
+        file = open(path + f'{tc}.out', 'w+', encoding='utf-8')
+        gen, period = solution(now)
+        w = file.writelines(f'{gen} {period}')
+        if gen >= 500: break
