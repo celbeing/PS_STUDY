32d59164d7850f150765525fbce71dc9d6754eed
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-09-18 00:46:20 +0900

    25.09.17.

--- Baekjoon Online Judge/1. Implementation/8. Math/9246.py
+++ Baekjoon Online Judge/1. Implementation/8. Math/9246.py
@@ -5,10 +5,10 @@ input = sys.stdin.readline
 
 a, b, c, d, e, f = map(float, input().split())
 sigma = (float(input()) ** 2) * 2
-a = 50 * (1 - exp(-a ** 2 / sigma))
-a += 25 * (exp(-a ** 2 / sigma) - exp(-b ** 2 / sigma))
-a += 10.5 * (exp(-b ** 2 / sigma) - exp(-c ** 2 / sigma))
-a += 31.5 * (exp(-c ** 2 / sigma) - exp(-d ** 2 / sigma))
-a += 10.5 * (exp(-d ** 2 / sigma) - exp(-e ** 2 / sigma))
-a += 21 * (exp(-e ** 2 / sigma) - exp(-f ** 2 / sigma))
+a = (50 * (1 - exp(-a ** 2 / sigma))
+     + 25 * (exp(-a ** 2 / sigma) - exp(-b ** 2 / sigma))
+     + 10.5 * (exp(-b ** 2 / sigma) - exp(-c ** 2 / sigma))
+     + 31.5 * (exp(-c ** 2 / sigma) - exp(-d ** 2 / sigma))
+     + 10.5 * (exp(-d ** 2 / sigma) - exp(-e ** 2 / sigma))
+     + 21 * (exp(-e ** 2 / sigma) - exp(-f ** 2 / sigma)))
 print(a)
