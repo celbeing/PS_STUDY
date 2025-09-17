523cf27f98f18e4bff7bc43ddbe6bf9e4b170096
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-09-17 23:26:21 +0900

    25.09.17.

--- Baekjoon Online Judge/test.py
+++ Baekjoon Online Judge/test.py
@@ -1,5 +1,13 @@
-n = int(input())
-s = list(input().strip())
-for i in range(n):
-    s[i] = 'L' if s[i] == 'l' else 'i'
-print(''.join(s))
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+field = [list(map(int, input().split())) for _ in range(n)]
+for i in range(1, m):
+    field[0][i] += field[0][i - 1]
+for i in range(1, n):
+    field[i][0] += field[i - 1][0]
+    for j in range(1, m):
+        field[i][j] += max(field[i - 1][j], field[i][j - 1])
+
+print(field[-1][-1])
