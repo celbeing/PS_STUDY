bb3ec95f645629dc1763ce7485f2e33a443ad469
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-09-19 07:42:51 +0900

    25.09.19.

--- Baekjoon Online Judge/test.py
+++ Baekjoon Online Judge/test.py
@@ -1,13 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-field = [list(map(int, input().split())) for _ in range(n)]
-for i in range(1, m):
-    field[0][i] += field[0][i - 1]
-for i in range(1, n):
-    field[i][0] += field[i - 1][0]
-    for j in range(1, m):
-        field[i][j] += max(field[i - 1][j], field[i][j - 1])
-
-print(field[-1][-1])
+n = int(input())
+a = list(map(int, input().split()))
+rage = 0
+total = 0
+for b in a:
+    rage += 1 if b else -1
+    total += rage
+print(total)
