985a56a9f9a135660bec176d46d89ad5d4c554a8
전라남도교육지원청 <kimsd1983@naver.com>
2025-09-20 21:36:44 +0900

    25.09.20.

--- Baekjoon Online Judge/5. Search/2. BFS/22868.py
+++ Baekjoon Online Judge/5. Search/2. BFS/22868.py
@@ -1,25 +1,48 @@
-# 22868: 산책(small)
+# 22868: 산책 (small)
 import sys
 from collections import deque
 input = sys.stdin.readline
-def solution():
-    n, m = map(int, input().split())
-    graph = [[] for _ in range(n + 1)]
-    for _ in range(m):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-    for i in range(1, n + 1):
-        graph[i].sort()
-    s, e = map(int, input().split())
-    bfs = deque([[s]])
-    while bfs:
-        now = bfs.popleft()
-        for next in graph[now[-1]]:
-            if next in now:
-                if e in now and next == s:
-                    print(len(now))
-                    exit()
-                else: continue
-            bfs.append(now + [next])
-solution()
+
+n, m = map(int, input().split())
+link = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    link[a].append(b)
+    link[b].append(a)
+for i in range(1, n + 1):
+    link[i].sort()
+s, e = map(int, input().split())
+
+dist = [-1] * (n + 1)
+dist[s] = 0
+goto = [i for i in range(n + 1)]
+dq = deque([s])
+while dq:
+    now = dq.popleft()
+    for nxt in link[now]:
+        if dist[nxt] == -1:
+            dist[nxt] = dist[now] + 1
+            dq.append(nxt)
+            goto[nxt] = now
+        if nxt == e:
+            dq.clear()
+            break
+
+visited = set()
+r = e
+while goto[r] != s:
+    visited.add(goto[r])
+    r = goto[r]
+
+dist = [-1] * (n + 1)
+dist[e] = 0
+dq.append(e)
+while dq:
+    now = dq.popleft()
+    for nxt in link[now]:
+        if nxt in visited or dist[nxt] > -1: continue
+        dist[nxt] = dist[now] + 1
+        dq.append(nxt)
+        if nxt == s:
+            print(dist[s] + len(visited) + 1)
+            exit()
