31399d1cf1cea60d00a916af08bb91131917a832
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2025-10-21 20:00:17 +0900

    25.10.21.

--- 2025 GTPC/2022 GTPC/C1.py
+++ 2025 GTPC/2022 GTPC/C1.py
@@ -1,1 +1,11 @@
 a, b, k = map(int, input().split())
+res = a // b
+a %= b
+for _ in range(k):
+    a *= 10
+    res *= 10
+    res += a // b
+    a %= b
+a *= 10
+if a // b >= 5: res += 1
+print(f'{res // 10**k}.{res%10**k}')
