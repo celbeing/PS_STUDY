db45266a2354b34be97d3687ca4fdef7d6a65d89
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-01-23 17:25:46 +0900

    26.01.23.

--- Coalla OJ/기초과정 연습문제/04.두 수의 나눗셈-2/test_case_gen.py
+++ Coalla OJ/기초과정 연습문제/04.두 수의 나눗셈-2/test_case_gen.py
@@ -5,9 +5,11 @@ path = r"C:\Users\kimsd\Documents\GitHub\PS_STUDY\Coalla OJ\기초과정 연습�
 
 for tc in range(1, 51):
     file = open(path + f'{tc}.in', 'w+', encoding='utf-8')
-    a, b = random.randint(0,10000000), random.randint(1,10000)
+    a = random.randint(1, 10000000)
+    b = random.randint(1, a)
     while (a, b) in check:
-        a, b = random.randint(0,10000000), random.randint(1,10000)
+        a = random.randint(1, 10000000)
+        b = random.randint(1, a)
     check.add((a, b))
     w = file.writelines(f'{a}\n{b}\n')
 
