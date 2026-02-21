cf4780bb0b3b008068535b3d572f23b28e8c5b48
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2026-02-21 19:38:58 +0900

    26.02.20.

--- Baekjoon Online Judge/test.py
+++ Baekjoon Online Judge/test.py
@@ -1,16 +1,26 @@
-k = int(input())
-a = int(input())
-small = large = a
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100001)
 
-t = 10 ** (k - 1)
-for _ in range(k - 1):
-    tmp = a // t
-    a %= t
-    a *= 10
-    a += tmp
-    if a > large:
-        large = a
-    elif a < small:
-        small = a
+n, p = map(int, input().split())
+a = [0] + list(map(int, input().split()))
+b = [0] + list(map(int, input().split()))
+tree = [set() for _ in range(n + 1)]
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    tree[u].add(v)
+    tree[v].add(u)
+check = [0] * (n + 1)
 
-print(large - small)
+def DFS(now):
+    check[now] = 1
+    need = 0
+    for nxt in tree[now]:
+        if check[nxt] == 0:
+            need += DFS(nxt)
+    need -= a[now] - b[now]
+    if need < 0: need = 0
+    return need
+
+result = DFS(p)
+print(result)
