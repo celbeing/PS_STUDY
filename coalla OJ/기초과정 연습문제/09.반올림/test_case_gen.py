119e2811fad6bbd8ab76bb6c79587cdab340b795
전라남도교육지원청 <kimsd1983@naver.com>
2026-03-02 15:05:06 +0900

    26.03.02.

---
+++ coalla OJ/기초과정 연습문제/09.반올림/test_case_gen.py
@@ -0,0 +1,26 @@
+import random
+random.random()
+check = set()
+path = r"C:\Users\kimsd\OneDrive\Documents\GitHub\PS_STUDY\coalla OJ\기초과정 연습문제\09.반올림\\"
+
+for tc in range(1, 51):
+    file = open(path + f'{tc}.in', 'w+', encoding='utf-8')
+    a = random.randint(1, 100000)
+    b = random.randint(1, a)
+
+    while (a, b) in check:
+        a = random.randint(1, 100000)
+        b = random.randint(1, a)
+    check.add((a, b))
+    t = a / b
+    c = random.randint(1, 100)
+    while round(t, c) == t:
+        c = random.randint(1, 100)
+
+    w = file.writelines(f'{a}\n{b}\n{c}\n')
+
+    res = round(t, c)
+    round()
+
+    file = open(path + f'{tc}.out', 'w+', encoding = 'utf-8')
+    w = file.writelines(f'{res}\n')
